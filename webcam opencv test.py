de454e2c194f937303354943b8cec2f4dbdfed35
Shicheng Zhao <chessplyr888@gmail.com>
2014-09-24 09:33:29 -0400

    findChessboardCorners

--- webcam opencv test.py
+++ webcam opencv test.py
@@ -3,6 +3,8 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 
+pattern_size = (7,7) # Inner corners of a chessboard
+
 while(True):
 	# Capture frame-by-frame
 	ret, frame = cap.read()
@@ -19,13 +21,18 @@ while(True):
 	# img = cv2.drawKeypoints(base, keypoints)
 
 	# Harris Corner Detector
-	base = np.float32(base) # Convert to float32 for cornerHarris
-	corner = cv2.cornerHarris(base, 2, 3, 0.04)
-	corner = cv2.dilate(corner, None) # Dilute to easily identify corners
-	frame[corner > 0.05*corner.max()] = [0, 0, 255]
-	img = frame
+	# base = np.float32(base) # Convert to float32 for cornerHarris
+	# corner = cv2.cornerHarris(base, 2, 3, 0.04)
+	# corner = cv2.dilate(corner, None) # Dilute to easily identify corners
+	# frame[corner > 0.05*corner.max()] = [0, 0, 255]
+	# img = frame
 
-	# TODO Draw Hough Lines with through corners
+	# findChessboardCorners + drawChessboardCorners
+	found, corners = cv2.findChessboardCorners(frame, pattern_size)
+	print found, corners
+
+	cv2.drawChessboardCorners(frame, pattern_size, corners, found)
+	
 
 	# Display the resulting frame
 	cv2.imshow('frame',img)
