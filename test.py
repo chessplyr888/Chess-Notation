4d2ce782d0e43df7f5f33c4aa9fd73dce3651655
Shicheng Zhao <chessplyr888@gmail.com>
2014-11-12 09:23:31 -0500

    catchup test.py

--- test.py
+++ test.py
@@ -8,7 +8,18 @@ import cv2
 
 
 
-# Return the left and right edges of a given row/col
+def checkBounds(axis, val):
+	if val < 0:
+			return 0
+	if axis is "x":
+		if val > cap.get(3):
+			return cap.get(3)
+	if axis is "y":
+		if val > cap.get(4):
+			return cap.get(4)
+	return val
+
+# Return the left/right or top/bottom edges of a given row/col
 def getEdgeCornersRows(corners):
 	length = len(corners)
 	last = length - 1
@@ -20,11 +31,16 @@ def getEdgeCornersRows(corners):
 
 	# print changeInX, changeInY, (changeInY/changeInX)
 
-	avgX = changeInX/length
-	avgY = changeInY/length
+	avgX = changeInX/last
+	avgY = changeInY/last
+
+	lowX = checkBounds("x", corners[0][0][0] - avgX)
+	lowY = checkBounds("y", corners[0][0][1] - avgY)
+	highX = checkBounds("x", corners[last][0][0] + avgX)
+	highY = checkBounds("y", corners[last][0][1] + avgY)
 
 	# Return a list of edges on opposite sides
-	return [[corners[0][0][0] - avgX, corners[0][0][1] - avgY], [corners[last][0][0] + avgX, corners[last][0][1] + avgY]]
+	return [[lowX, lowY], [highX, highY]]
 
 def getEdgeCornersCols(corners):
 	length = len(corners)
@@ -33,13 +49,20 @@ def getEdgeCornersCols(corners):
 	changeInX = corners[last][0] - corners[0][0]
 	changeInY = corners[last][1] - corners[0][1]
 
-	avgX = changeInX/length
-	avgY = changeInY/length
+	avgX = changeInX/last
+	avgY = changeInY/last
 
-	return [[corners[0][0] - avgX, corners[0][1] - avgY], [corners[last][0] + avgX, corners[last][1] + avgY]]
+	lowX = checkBounds("x", corners[0][0] - avgX)
+	lowY = checkBounds("y", corners[0][1] - avgY)
+	highX = checkBounds("x", corners[last][0] + avgX)
+	highY = checkBounds("y", corners[last][1] + avgY)
+
+	# Return a list of edges on opposite sides
+	return [lowX, lowY], [highX, highY]
 
 # Return a list of all the corers, including the edges of the board
 def edgeCorners(corners):
+	# Corners should be an nparray of shape (49, 1, 2)
 	newCorners = []
 
 	print "ROWS"
@@ -91,7 +114,7 @@ def edgeCorners(corners):
 
 	# Convert newCorners to numpy array
 	
-	print len(cols), len(cols[0])
+	# print len(cols), len(cols[0])
 
 	for i in range(0, len(cols)):
 		for j in range(0, len(cols[i])):
@@ -99,9 +122,37 @@ def edgeCorners(corners):
 
 	# print newCorners, len(newCorners)
 
-	fullCorners = np.array(newCorners)
+	fullCorners = np.array([newCorners])
 
+	# fullCorners should be an nparray of shape (64, 1, 2)
 	return fullCorners
+
+
+def getContourList(corners):
+	contours = []
+	for i in range(0, 8):
+		for j in range(0, 7):
+			topLeft = corners[0][(i)*9 + (j)]
+			topRight = corners[0][(i)*9 + (j + 1)]
+			botLeft = corners[0][(i + 1)*9 + (j)]
+			botRight = corners[0][(i + 1)*9 + (j + 1)]
+			contours.append(np.array(topLeft, topRight, botLeft, botRight, dtype = numpy.int32))
+
+	return contours
+
+# KALYAN/SATHVIK FIND COLOR OF THE SQUARE
+def getSquareColor(points):
+	white=0
+	black=0
+	for i in points:
+		if i[0]==0: black+=1
+		else: white+=1
+
+	return white>black
+
+
+# SHICHENG GET PIXELS
+# GET BINARY MASK
 
 
 
@@ -122,6 +173,21 @@ print fullCorners
 
 if found is True:
 	cv2.drawChessboardCorners(frame, pattern_size, fullCorners, found)
+
+	# Draws the corners
+	for i in range(0, len(fullCorners[0])):
+		print fullCorners[0][i]
+		x = int(fullCorners[0][i][0])
+		y = int(fullCorners[0][i][1])
+		cv2.circle(frame, (x, y), 5, (0, 255, 0), -1) # Green Points
+
+	# Get the contours
+	contours = getContourList(fullCorners)
+
+	# Draw the contours
+	cv2.drawContours(frame, contours, -1, (0, 0, 255), 3) # Blue Contours
+
+	# Get the average color of the square
 
 # Display the resulting frame
 cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
